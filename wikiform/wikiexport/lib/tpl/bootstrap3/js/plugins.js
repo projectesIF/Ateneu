d44e417236110e8cd7f162b6efeac8f903486b84
Ulises Martínez <umartinez@seidor.es>
2017-04-28 09:30:49 +0200

    cambio de tema y fix de iconos en PDFs autogenerados

---
+++ wikiform/wikiexport/lib/tpl/bootstrap3/js/plugins.js
@@ -0,0 +1,30 @@
+/*!
+ * DokuWiki Bootstrap3 Template: Plugins Hacks!
+ *
+ * Home     http://dokuwiki.org/template:bootstrap3
+ * Author   Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * License  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+jQuery(document).on('bootstrap3:plugins', function(e) {
+
+  setTimeout(function() {
+
+    /* DOKUWIKI:include js/plugins/csv.js */
+    /* DOKUWIKI:include js/plugins/data.js */
+    /* DOKUWIKI:include js/plugins/datatables.js */
+    /* DOKUWIKI:include js/plugins/davcal.js */
+    /* DOKUWIKI:include js/plugins/discussion.js */
+    /* DOKUWIKI:include js/plugins/explain.js */
+    /* DOKUWIKI:include js/plugins/gallery.js */
+    /* DOKUWIKI:include js/plugins/include.js */
+    /* DOKUWIKI:include js/plugins/inlinetoc.js */
+    /* DOKUWIKI:include js/plugins/publish.js */
+    /* DOKUWIKI:include js/plugins/tag.js */
+    /* DOKUWIKI:include js/plugins/tagging.js */
+    /* DOKUWIKI:include js/plugins/translation.js */
+    /* DOKUWIKI:include js/plugins/wrap.js */
+
+  }, 0);
+
+});
