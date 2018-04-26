d8b48bd717b8427cfdfb5ef7c9ea2c89a3cb09c7
Salva Valldeoriola <salva9@gmail.com>
2018-04-26 13:04:32 +0200

    Reformat code structure

--- wikiform/index.php
+++ wikiform/index.php
@@ -1,3 +1,8 @@
-<?php 
-header ("Location: wikiexport/"); 
-?>
+<?php
+/**
+ * Forwarder to doku.php
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
+ */
+header("Location: doku.php");
